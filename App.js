2ce5f5f884512cf49bdb90089dac08730e052961
keerthi379 <135863582+keerthi379@users.noreply.github.com>
2023-12-24 23:47:02 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from "./Header";
+import Products_list from "./Products_list";
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Header/>
+
+      <Products_list/>
+      
+    </div>
+  );
+}
+
+export default App;
